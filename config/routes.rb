a0f56fb54c1cceea40979ebf48609dc831c71085
Jennifer Gomez <jennifergomezbruno@gmail.com>
2020-03-10 16:59:46 -0400

    got relationships working

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users
+  resources :locations
+
+  #~~~~~~~favorites routes~~~~~~~#
+  post '/favorites', to: 'locations#favorites'
+  delete '/favorites/:user_id/:location_id', to: 'locations#destroy'
+
+  #~~~~~~~reviews routes~~~~~~~~#
+  post '/reviews', to: 'locations#reviews'
+
+  #~~~~~~~ratings routes~~~~~~~~#
+  post '/ratings', to: 'locations#ratings'
+
+ 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,30 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'rest-client'
+require 'json'
+require 'byebug'
+
+Location.destroy_all
+User.destroy_all
+
+key = Rails.application.credentials[:API]
+
+response_yelp = RestClient.get("https://api.yelp.com/v3/businesses/search?location=Washington D.C.&limit=50&categories=publicservicesgovt", {"Authorization" => "Bearer #{key}"})
+
+response = JSON.parse(response_yelp)
+
+response_array = response["businesses"]
+
+response_array.each do |location|
+    name = location["name"]
+    img_url = location["image_url"]
+    url = location["url"]
+    phone = location["display_phone"]
+    address = location["location"]["display_address"][0]
+    
+    Location.create(name: name, img_url: img_url, url:url, phone:phone, address: address)
+end
+
+user1 = User.create(username: "CatTheGreat", name: "Cat Walker", avatar:"https://image.flaticon.com/icons/png/512/194/194938.png")
+
+location1 = Location.first.id
+
+Review.create(user_id: user1.id, location_id: location1, review: "Would return.")
